352c4336d972799c2088d9ac0419c40169e47646
paulenerelano <paulene.relano@gmail.com>
2019-05-01 18:43:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -42,6 +42,15 @@ app.use(function(err, req, res, next) {
 /* Connect DB */
 dbConfig.DB.dbConnect();
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 /* Listen to port */
 app.listen(port, function() {
   console.log("Server is running on Port:", port);
